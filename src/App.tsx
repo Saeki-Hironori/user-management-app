ff62e11df4556d4ec21267cd8bdb8066c79107c7
Saeki Hironori <saeki.hironori@itfllc.co.jp>
2023-04-05 18:07:29 +0900

    ルーティング作成途中

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import theme from "./theme/theme";
+import { Router } from "./router/Router";
+import { HomeRoutes } from "./router/HomeRoutes";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Button colorScheme="teal">ボタン</Button>
+        <p>あああああああああ</p>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
